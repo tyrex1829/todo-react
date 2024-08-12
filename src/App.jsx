c77894a1d9dca545b3f3908b7b546ef7e8969ada
tyrex1829 <sakshamkhare19@gmail.com>
2024-08-12 15:50:21 +0530

    complete todolist

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Todo from "./components/Todo";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <h1>Todo App</h1>
-    </>
+    <div>
+      <Todo />
+    </div>
   );
 }
 
